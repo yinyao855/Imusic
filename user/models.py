b62e69f101aeda54f51c5141fc2a44ec2d7fa2f5
yy <1311095683@qq.com>
2024-04-21 00:04:02 +0800

    处理静态资源+5

--- user/models.py
+++ user/models.py
@@ -10,7 +10,7 @@ class User(models.Model):
     password = models.CharField(max_length=50)
     registration_date = models.DateTimeField(auto_now_add=True)
     bio = models.TextField("用户简介", blank=True, null=True)
-    avatar = models.ImageField(upload_to='media/'+'avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     # 表示在数据库存储的是user，但是在显示的时候显示的是User
     role_choices = (
         ('user', 'User'),
