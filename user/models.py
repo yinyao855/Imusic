92050a1b5d33079c5d57ba9b4083d0d45e7ac88b
yy <1311095683@qq.com>
2024-04-20 23:41:44 +0800

    处理静态资源+1

--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from imusic.settings import STATIC_URL
+from imusic.settings import MEDIA_URL
 
 # Create your models here.
 class User(models.Model):
@@ -10,7 +10,7 @@ class User(models.Model):
     password = models.CharField(max_length=50)
     registration_date = models.DateTimeField(auto_now_add=True)
     bio = models.TextField("用户简介", blank=True, null=True)
-    avatar = models.ImageField(upload_to=STATIC_URL+'avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to=MEDIA_URL+'avatars/', blank=True, null=True)
     # 表示在数据库存储的是user，但是在显示的时候显示的是User
     role_choices = (
         ('user', 'User'),
