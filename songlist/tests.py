668c7f429c5e61af9d7176412e5da92e33d1519f
sivenlu <sivenlu2003@gmail.com>
2024-05-30 11:06:00 +0800

    see description get detailed info
    finish: create songlist
    
    you should also create the medir dir just like the description of my
    last commit.

--- songlist/tests.py
+++ songlist/tests.py
@@ -1,5 +1,9 @@
+import os
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.conf import settings
+
 from .models import User, SongList
 from user.tests import generate_token
 
@@ -58,3 +62,115 @@ class GetUserSonglistsTests(TestCase):
         self.songlist1.delete()
         self.songlist2.delete()
 
+
+class SonglistCreateTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('create')
+        self.user = User.objects.create(
+            username='testuser',
+            email='testuser@example.com',
+            password='password'
+        )
+        self.token = generate_token('testuser', 'user')
+
+        # 使用本地文件
+        self.cover_path = os.path.join(settings.MEDIA_ROOT, 'covers/test.jpg')
+
+        with open(self.cover_path, 'rb') as f:
+            self.cover = SimpleUploadedFile('cover.jpg', f.read(), content_type='image/jpeg')
+
+    def test_songlist_create_missing_fields(self):
+        data = {
+            'title': 'Test Songlist',
+        }
+        response = self.client.post(self.url, data, HTTP_AUTHORIZATION=self.token)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'success': False, 'message': '缺少字段：owner'})
+
+    def test_songlist_create_missing_cover(self):
+        data = {
+            'title': 'Test Songlist',
+            'owner': 'testuser'
+        }
+        response = self.client.post(self.url, data, HTTP_AUTHORIZATION=self.token)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'success': False, 'message': '缺少封面图'})
+
+    def test_songlist_create_existing_songlist(self):
+        SongList.objects.create(
+            title='Test Songlist',
+            owner=self.user,
+            cover=self.cover
+        )
+        data = {
+            'title': 'Test Songlist',
+            'owner': 'testuser',
+            'cover': self.cover
+        }
+        files = {
+            'cover': self.cover
+        }
+        response = self.client.post(self.url, data, HTTP_AUTHORIZATION=self.token, files=files)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'success': False, 'message': '歌单已存在'})
+
+    def test_songlist_create_owner_not_exist(self):
+        data = {
+            'title': 'Test Songlist',
+            'owner': 'nonexistentuser',
+            'cover': self.cover
+        }
+        files = {
+            'cover': self.cover
+        }
+        response = self.client.post(self.url, data, HTTP_AUTHORIZATION=self.token, files=files)
+        self.assertEqual(response.json(), {'success': False, 'message': '所有者不存在'})
+        self.assertEqual(response.status_code, 404)
+
+    def test_songlist_create_success(self):
+        data = {
+            'title': 'Test Songlist',
+            'owner': 'testuser',
+            'tag_language': '国语',
+            'introduction': 'This is a test songlist',
+            'cover': self.cover
+        }
+        files = {
+            'cover': self.cover
+        }
+        response = self.client.post(self.url, data, HTTP_AUTHORIZATION=self.token, files=files)
+        self.assertEqual(response.json()['message'], '歌单创建成功')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()['success'], True)
+        self.assertTrue(SongList.objects.filter(title='Test Songlist', owner=self.user).exists())
+
+    def test_songlist_create_missing_token(self):
+        data = {
+            'title': 'Test Songlist',
+            'owner': 'testuser',
+            'cover': self.cover
+        }
+        files = {
+            'cover': self.cover
+        }
+        response = self.client.post(self.url, data, files=files)
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {'success': False, 'message': 'Authorization header missing'})
+
+    def test_songlist_create_invalid_token(self):
+        data = {
+            'title': 'Test Songlist',
+            'owner': 'testuser',
+            'cover': self.cover
+        }
+        files = {
+            'cover': self.cover
+        }
+        response = self.client.post(self.url, data, HTTP_AUTHORIZATION='Bearer invalid_token', files=files)
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {'success': False, 'message': 'Invalid token'})
+
+    def tearDown(self):
+        self.user.delete()
+        SongList.objects.all().delete()
